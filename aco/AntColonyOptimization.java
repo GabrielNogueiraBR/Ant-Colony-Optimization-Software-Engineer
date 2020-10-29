adca3bcc164a68efbe18af86ca62b88256d9c0e2
Gabriel Nogueira <gabrielnoog7@gmail.com>
2020-10-29 12:26:56 -0300

    Update AntColonyOptimization.java

--- aco/AntColonyOptimization.java
+++ aco/AntColonyOptimization.java
@@ -1,5 +1,7 @@
 package aco;
 
+
+import java.util.ArrayList;
 import java.util.List;
 
 //Classe Ant Colony Optimization
@@ -7,7 +9,66 @@ public class AntColonyOptimization {
      
      public static void run(List<Cidade> listaCidade, int numeroMaximoIteracao, Double influenciaFeromonio, Double influenciaDistancia, Double taxaEvaporacaoFeromonio, Double valorInicialFeromonio, Double constanteAtualizacaoFeromonio) {
         
+        List<Rota> listaRota;
+        int iteracao = 0;
+
+        //definir a tabela de rotas
+        listaRota = constroiRotas(listaCidade,valorInicialFeromonio);
         
+        //atualizando a tabela de probabilidades de escolha
+        for (Rota rota : listaRota) {
+            rota.atualizaProbabilidadeEscolha(listaRota);
+        }
+        
+        //colocar um formiga em cada cidade
+        for (Cidade cidade : listaCidade) {
+            cidade.adicionaFormiga(constanteAtualizacaoFeromonio);           
+        }
+
+        while(iteracao < numeroMaximoIteracao){
+            //System.out.println("Tamanho da lista de Rota" + listaRota.size());
+
+            for (Cidade cidade : listaCidade) {
+                //System.out.println("\n\n\n\n");
+                //faz a formiga percorrer todas as rotas com base na cidade de origem
+                cidade.getFormiga().run(listaRota, cidade.getFormiga().getCidadeOrigem());
+
+                //atualiza a distancia percorrida por cada formiga
+                cidade.getFormiga().defineDistanciaPercorrida();
+
+                // //exibe a rota percorrida pela formiga
+                // System.out.println(cidade.getFormiga().exibeRotaPercorrida());
+                // System.out.println("Distancia Percorrida: " + cidade.getFormiga().getDistanciaPercorrida());
+
+                //exibe a rota percorrida pela formiga da cidade 1
+                if(cidade.equals(listaCidade.get(0))){
+                    System.out.println(cidade.getFormiga().exibeRotaPercorrida());
+                    System.out.println("Distancia Percorrida: " + cidade.getFormiga().getDistanciaPercorrida());    
+                }
+                
+            }
+
+            //realizar a evaporacao do feromonio em todas as rotas e retorna a lista de rotas atualizadas
+            listaRota = Rota.realizaEvaporacaoFeromonioRotas(listaRota, taxaEvaporacaoFeromonio, listaCidade);
+            
+            iteracao++;
+        }        
+    }
+
+    public static List<Rota> constroiRotas(List<Cidade> cidades, Double valorInicialFeromonio){
+        List<Rota> novasRotas = new ArrayList<Rota>();
+
+        for (Cidade cidadeOrigem : cidades) {
+            for (Cidade cidadeDestino : cidades) {
+                if(cidadeDestino != cidadeOrigem){
+                    Rota novaRota = new Rota(cidadeOrigem, cidadeDestino, valorInicialFeromonio);
+                    novasRotas.add(novaRota);
+                }
+                
+            }
+        }
+
+        return novasRotas;
     }
 
 }
